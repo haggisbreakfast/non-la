a5478ffbc554a45178c504411bfe6366093a8709
Lindsay Stewart <lj.stewart00@gmail.com>
2020-02-21 16:32:07 -0800

    adds style tweaks, color cycle animation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Header></Header>
+      <Header>NON LA</Header>
       <PageContainer>
         <About>
           <p>
--- src/Components.js
+++ src/Components.js
@@ -1,6 +1,6 @@
 // import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-import banner from "./banner.png";
+// import banner from "./banner.png";
 import background from "./nonla.jpg";
 
 export const GlobalStyle = createGlobalStyle`
@@ -9,33 +9,63 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 24px;
     font-weight: bold;
     color: grey;
-     margin: auto;
-    height: 100%;
-
-      height: 100vh;
-
-
-
-
+    margin: auto;
+    height: 100vh;
+    @media(max-width: 800px){
+      font-size: 18px;
+    }
   }
 `;
 
 export const Header = styled.header`
-  background: url(${banner}) no-repeat center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-size: cover;
   position: fixed;
   top: 0;
   left: 0;
-  height: 220px;
+  height: 20vw;
   margin-bottom: 10px;
   z-index: 1;
   width: 100%;
+  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 2s; /* Firefox < 16 */
+  -ms-animation: fadein 2s; /* Internet Explorer */
+  -o-animation: fadein 2s; /* Opera < 12.1 */
+  animation: fadein 2s;
+  font-size: 48px;
+  font-weight: bold;
+  color: white;
+  opacity: 0.9;
+  animation: colorchange 10s;
+  -webkit-animation: colorchange 10s;
+  -webkit-animation-iteration-count: infinite;
+	animation-iteration-count: infinite;
+      @keyframes colorchange
+    {
+      0%   {background: #FF9AA2;}
+      16.666%  {background:  #FFB7B2;}
+      33.333%  {background: #FFDAC1;}
+      49.999%  {background: #E2F0CB;}
+      66.666%  {background: #B5EAD7;}
+      100% {background: #C7CEEA;}
+    }
+
+    @-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */
+    {
+      0%   {background: #FF9AA2;}
+      16.666%  {background:  #FFB7B2;}
+      33.333%  {background: #FFDAC1;}
+      49.999%  {background: #E2F0CB;}
+      66.666%   {background: #B5EAD7;}
+      100% {background: #C7CEEA;}
+    }
 `;
-// export const
+
 export const PageContainer = styled.div`
   @import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
   background: url(${background}) no-repeat;
-  background-position: right -220px;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -46,7 +76,13 @@ export const PageContainer = styled.div`
   height: 1300px;
   overflow: hidden;
   padding: 0 60px;
+  @media(max-width: 800px){
+      align-items: flex-start;
+    }
+
 `;
+
+
 export const About = styled.div`
   color: white;
   max-width: 33%;
@@ -57,6 +93,12 @@ export const About = styled.div`
   -ms-animation: fadein 2s; /* Internet Explorer */
   -o-animation: fadein 2s; /* Opera < 12.1 */
   animation: fadein 2s;
+  @media(max-width: 800px){
+  max-width: 100%;
+  align-items: flex-start;
+  padding-top: 40%;
+
+  }
 
   @keyframes fadein {
     from {
