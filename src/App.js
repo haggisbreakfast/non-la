7eba4fb9f630dba9d447fb7cffdc14c8a9894b63
Lindsay Stewart <lindsay.stewart@briteweb.com>
2024-02-21 00:18:21 -0800

    update layout and styles for desktop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
-import { GlobalStyle, Header, About, PageContainer, Link, Image, SocialContainer } from "./Components";
+import { GlobalStyle, Header, About, PageContainer, Link, Image, SocialContainer, Portrait, Bio } from "./Components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook, faInstagram, faSpotify, faBandcamp } from '@fortawesome/free-brands-svg-icons';
@@ -11,12 +11,13 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Header></Header>
       <PageContainer>
-        <About>
-          <p>
-            Debut album Not in Love on <Link href="https://nonla.bandcamp.com/album/not-in-love">Kingfisher Bluez</Link> and <Link href="https://lostsoundtapes.com/collections/lost-sound-tapes-releases/products/non-la-not-in-love-cassette-tape">Lost Sound Tapes</Link>.
-            </p>
+      <Header></Header>
+
+      <About>
+
+      <Bio>
+      <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit malesuada praesent penatibus sociosqu luctus.</p>
             <SocialContainer>
               <Link href="mailto:nonlaband@gmail.com" target="blank"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></Link>
               <Link href="https://www.facebook.com/nonlaband/" target="blank"><FontAwesomeIcon icon={faFacebook} /></Link>
@@ -24,11 +25,17 @@ function App() {
               <Link href="https://open.spotify.com/album/5vFw06omAAEVLo7JTobZcp?si=nktnYZFZSEOk4zmbSkwlrQ" target="blank"><FontAwesomeIcon icon={faSpotify} /></Link>
               <Link href="https://nonla.bandcamp.com/album/not-in-love" target="blank"><FontAwesomeIcon icon={faBandcamp} /></Link>
             </SocialContainer>
+        </Bio>
+
+       <Portrait>
+       </Portrait>
+
+
 
         </About>
         <Image></Image>
       </PageContainer>
-    </div >
+    </div>
   );
 }
 
--- src/Components.js
+++ src/Components.js
@@ -1,7 +1,8 @@
 // import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import banner from "./banner-transparent.png";
-import background from "./nonla-min.jpg";
+import rocks from "./rocks.png";
+import portrait from "./portrait.png";
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -21,12 +22,12 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-size: cover;
+  /* background-size: cover; */
   position: fixed;
-  top: 0;
+  top: 2rem;
   left: 0;
-  height: 14vw;
-  margin-bottom: 10px;
+  /* height: 14vw; */
+  /* margin-bottom: 10px; */
   z-index: 1;
   width: 100%;
   -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
@@ -36,17 +37,20 @@ export const Header = styled.header`
   animation: fadein 2s;
   font-size: 48px;
   font-weight: bold;
-  color: white;
-  opacity: 0.9;
-  animation: colorchange 10s;
+  /* color: white; */
+  /* opacity: 0.9; */
+  /* background: url(${portrait}); */
+  background-size: contain;
+
+  /* animation: colorchange 10s;
   -webkit-animation: colorchange 10s;
   -webkit-animation-iteration-count: infinite;
-	animation-iteration-count: infinite;
+	animation-iteration-count: infinite; */
   @media(max-width: 650px) {
     height: 18vw;
 
   }
-      @keyframes colorchange
+      /* @keyframes colorchange
     {
       0%   {background: #FF9AA2;}
       16.666%  {background:  #FFB7B2;}
@@ -57,22 +61,22 @@ export const Header = styled.header`
     }
 
     @-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */
-    {
+    /* {
       0%   {background: #FF9AA2;}
       16.666%  {background:  #FFB7B2;}
       33.333%  {background: #FFDAC1;}
       49.999%  {background: #E2F0CB;}
       66.666%   {background: #B5EAD7;}
       100% {background: #C7CEEA;}
-    }
+    } */ 
 &:before {
   content: '';
   background: url(${banner}) no-repeat;
   background-position: center;
   background-size: 100%;
-  width: 360px;
-  height: 140px;
-  margin-top: 20px;
+  width: 80%;
+  height: 16rem;
+  /* margin-top: 20px; */
   @media(max-width: 650px){
 
   width: 200px;
@@ -84,18 +88,17 @@ export const Header = styled.header`
 
 export const PageContainer = styled.div`
   @import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
-  background: url(${background}) no-repeat;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background-position: 25% 25%;
+  background-color: #F9DAD0;
+  /* background-size:cover; */
+  /* background-position: center bottom; */
+  /* background: url(${rocks}); */
+  background-size: 40rem;
   display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  height: 1300px;
+  justify-content: center;
+  align-items: flex-start;
+
   overflow: hidden;
-  padding: 0 60px;
+  /* padding: 0 60px; */
   @media(max-width: 650px){
       align-items: flex-start;
     }
@@ -104,17 +107,21 @@ export const PageContainer = styled.div`
 
 
 export const About = styled.div`
-  color: white;
-  max-width: 30%;
-  overflow-wrap: break-word;
+  /* color: #FF2E00; */
+  display: flex;
+  align-items: center;
+  width: 100%;  
+  /* justify-content: space-between; */
+  /* gap: 4rem; */
+  /* max-width: 30%; */
   text-align: left;
+  /* height: 120vh; */
   -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
   -moz-animation: fadein 2s; /* Firefox < 16 */
   -ms-animation: fadein 2s; /* Internet Explorer */
   -o-animation: fadein 2s; /* Opera < 12.1 */
   animation: fadein 2s;
-  padding-bottom: 30%;
-  padding-right: 10%;
+  /* padding-bottom: 30%; */
   @media(max-width: 650px){
   align-items: flex-start;
   padding-top: 30%;
@@ -173,12 +180,32 @@ export const About = styled.div`
   }
 `;
 
+export const Portrait = styled.div`
+background: url(${portrait}) no-repeat;
+background-size: cover;
+background-position: center;
+height: 120vh;
+width: 50%;
+/* box-shadow: 0px 3px 15px #B36A5E; */
+`
+
+export const Bio = styled.div`
+/* background: url(${rocks}) no-repeat; */
+/* background-size: cover; */
+width: 50%;
+/* max-height: 12rem; */
+color: #FF2E00;
+font-size: 1.75rem;
+padding: 10%;
+`
+
 export const SocialContainer = styled.div`
 display: flex;
 
 a {
   padding-right: 10px;
-  font-size: 20px;
+  font-size: 40px;
+  color:#8FBBDB;
   @media (max-width: 650px) {
     font-size: 16px;
     padding-right: 5px;
@@ -189,7 +216,7 @@ a {
 
 
 export const Link = styled.a`
-color: white;
+/* color: white; */
 text-decoration: none;
 &:hover {
   color: #B5EAD7;
